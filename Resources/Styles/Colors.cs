1bb49f2af229c97038037c222e5904b1d8cee61a
Felipe Baltazar <felipe.dasilvabaltazar@gmail.com>
2023-08-20 14:56:14 -0300

    Using resources

---
+++ Resources/Styles/Colors.cs
@@ -0,0 +1,9 @@
+﻿namespace Maui.Ideas.App.Resources.Styles;
+
+public partial class Colors : ResourceDictionary
+{
+    public Colors()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Resources/Styles/Styles.cs
@@ -0,0 +1,17 @@
+﻿namespace Maui.Ideas.App.Resources.Styles;
+
+public partial class ApplicationStyles : ResourceDictionary
+{
+    public ApplicationStyles(params ResourceDictionary[] merged)
+    {
+        foreach(var inner in merged)
+            MergedDictionaries.Add(inner);
+
+        InitializeComponent();
+    }
+
+    public ApplicationStyles()
+    {
+        InitializeComponent();
+    }
+}
