aeada4ad0489657758d335b3491fdb5dc00cd9f9
Felipe Baltazar <felipe.dasilvabaltazar@gmail.com>
2024-01-08 09:31:14 -0300

    Create OtherViewModel.cs

---
+++ Maui.Ideas.App/Presentation/ViewModels/Pages/OtherViewModel.cs
@@ -0,0 +1,39 @@
+﻿using Maui.Ideas.App.Abstractions;
+using Microsoft.Extensions.Logging;
+
+namespace Maui.Ideas.App.Presentation.ViewModels.Pages;
+
+public class OtherViewModel : BaseViewModel
+{
+    public OtherViewModel(
+        ILazyDependency<ILoaderService> loaderService,
+        ILazyDependency<INavigationService> navigationService,
+        IMainThreadService mainThreadService,
+        ILogger logger)
+        : base(
+            loaderService,
+            navigationService,
+            mainThreadService,
+            logger)
+    {
+    }
+
+    public async Task DoSomethingAsync()
+    {
+        var deviceInfo = DeviceInfo.Current;
+        var manufacturer = deviceInfo.Manufacturer;
+
+        if (deviceInfo.Platform == DevicePlatform.Android)
+        {
+            await NavigationService.NavigateToAsync($"androidPage?manufacturer={manufacturer}").ConfigureAwait(false);
+        }
+        else if (deviceInfo.Platform == DevicePlatform.iOS)
+        {
+            await NavigationService.NavigateToAsync($"iosPage?manufacturer={manufacturer}").ConfigureAwait(false);
+        }
+        else
+        {
+            throw new NotImplementedException("Unknow platform");
+        }
+    }
+}
